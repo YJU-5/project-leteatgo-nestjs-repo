7b581d7a78228ace4da0ba2bb001aff91411f103
KimHyeongSun445 <gudtjs1004sd@naver.com>
2025-02-14 18:36:03 +0900

    🚀 FEAT: 새로운 기능 추가
    - DB에 있는 테이블을 NestJs 엔티티로 변환하는 작업, 현재 chat-room과
      user를 완료함
    [ ✨ Feature ] 기능 개발 하기전 엔티티 구축 및 설계  #5

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,61 @@
+import { ChatRoom } from "src/chat-room/entities/chat-room.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryColumn } from "typeorm";
+@Entity()
+export class User {
+
+    // 유저아이디
+    @PrimaryColumn('uuid')
+    id:string;
+
+    // 이름
+    @Column()
+    name:string;
+
+    // 이메일 
+    @Column()
+    email:string;
+
+    // 전화번호 
+    @Column({name:'phone_number'}) //스네이크 컬럼명 매핑
+    phoneNumber:string;
+
+    // 생년월일 
+    @Column('date')
+    birthday:Date;
+
+    // 성별 
+    @Column('enum',{enum:['M','F']})
+    gender:string;
+
+    // 사진 
+    @Column({name:'picture_url',nullable:true})
+    pictureUrl:string;
+
+    // 소개문 
+    @Column({nullable:true})
+    description:string;
+
+    // 유저역할 
+    @Column('enum',{enum:['user','admin']})
+    role:string;
+
+    // 소셜로그인 제공자 
+    @Column('enum',{enum:['GOOGLE','KAKAO','INSTAGRAM'],name:'social_provider'})
+    socialProvider:string;
+
+    // 소셜로그인 고유ID
+    @Column({name:'social_id'})
+    socialId:string;
+
+    // 등록날짜
+    @CreateDateColumn({name:'created_at'}) // 생성일시 자동 생성 
+    createdAt: Date;
+
+    // 수정날짜
+    @CreateDateColumn({name:'updated_at'})
+    updatedAt:Date;
+
+    // ChatRoom과 조인 
+    @OneToMany(()=>ChatRoom,(chatRoom) => chatRoom.hostId)
+    chatRooms:ChatRoom[];
+}
