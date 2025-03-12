1f7322fa8caf3c30db4bcd62c896f75c9bdf521d
SeungHyun CHA <cjhms002@naver.com>
2025-03-13 00:45:11 +0900

    edit/docker-file-and-github-action

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,12 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const typeOrmModuleOptions: TypeOrmModuleOptions = {
-  type: process.env.DB_TYPE as 'postgres', // postgres db를 명시
+  type: 'postgres', // postgres db를 명시
   host: process.env.DB_HOST, // postgres host
-  port: parseInt(process.env.DB_PORT,10), // postgres port
+  port: parseInt(process.env.DB_PORT, 10), // postgres port
   username: process.env.DB_USERNAME, // db username
   password: process.env.DB_PASSWORD, // db password
   database: process.env.DB_DATABASE_NAME, // database name
   entities: [__dirname + '/../**/*.entity.{js,ts}'], // entity class를 기반으로 테이블을 생성할 수 있도록 entity 파일 규칙 정의
   synchronize: true,
-}; 
+};
